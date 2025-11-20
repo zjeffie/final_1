39c1b60c45e17d047c360fd4dfffa3b85fa0987d
zjeffie <3122647287@qq.com>
2025-11-19 19:35:51 -0500

    Add reproducibility features: Makefile install rule, renv environment, updated README

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
+all: output/report.html
+
+output/report.html: code/01_load_data.R code/02_clean_data.R code/03_make_tables.R code/04_make_figures.R code/06_render_report.R data/fetal_health.csv
+	Rscript code/06_render_report.R
+
 install:
 	R -e "renv::restore(prompt = FALSE)"
+
--- code/04_make_figures.R
+++ code/04_make_figures.R
@@ -6,4 +6,5 @@ if (length(num_cols) == 0) stop("No numeric columns to plot.")
 
 xvar <- num_cols[1]
 png("output/figure_histogram.png", width = 800, height = 500)
-hist(clean[[xvar]], main = paste("Distribution of", xut/figure_histogram.png")
+hist(clean[[xvar]], main = paste("Distribution of", xvar), xlab = xvar)
+
